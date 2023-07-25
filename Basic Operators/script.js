5b0cde139db986eb85338bc9c726e1aaf0d57f87
Stoyan Petrov <stoyanpetrov388@yahoo.com>
2023-07-25 17:05:33 +0300

    Add files via upload

---
+++ Basic Operators/script.js
@@ -0,0 +1,28 @@
+const now = 2037;
+const ageStoyan = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageStoyan, ageSarah);
+
+console.log(ageStoyan * 2, ageStoyan / 10, 2 ** 3);
+
+const firstName = "Stoyan";
+const lastName = "Petrov";
+console.log(firstName + " " + lastName);
+
+//Assignment operators
+let x = 10 + 5;
+x += 10;
+x += 4;
+x++;
+x--;
+x--;
+console.log(x);
+
+//Comparison operators
+console.log(ageStoyan > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+
